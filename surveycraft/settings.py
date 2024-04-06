242d6cd7ebf5f2cb1221e83e5d697af362fc23d2
masahiro0000 <aya0724masa@gmail.com>
2024-04-06 22:09:02 +0900

    デプロイ準備

--- surveycraft/settings.py
+++ surveycraft/settings.py
@@ -19,7 +19,7 @@ environ.Env.read_env()
 
 DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
-ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
