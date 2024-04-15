a2bb2df823511b716bd262129200afb8c1ef8636
masahiro0000 <aya0724masa@gmail.com>
2024-04-15 11:36:53 +0900

    model変更

--- survey/models.py
+++ survey/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from accounts.models import Profile
+
+class Survey(models.Model):
+    user = models.ForeignKey(Profile, related_name="surveys", on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
 
 class Question(models.Model):
     TEXT = 'TX'
@@ -6,12 +15,13 @@ class Question(models.Model):
     MULTIPLE_CHOICE = 'MC'
     RATING_SCALE = 'RS'
     QUESTION_TYPE = [
-        (TEXT, 'Text'),
-        (SINGLE_CHOICE, 'Single choice'),
-        (MULTIPLE_CHOICE, 'Multiple choice'),
-        (RATING_SCALE, 'Rating scale'),
+        (TEXT, 'テキスト入力'),
+        (SINGLE_CHOICE, 'ラジオボタン'),
+        (MULTIPLE_CHOICE, '複数選択可'),
+        (RATING_SCALE, '評価スケール'),
     ]
 
+    survey = models.ForeignKey(Survey, related_name="questions", on_delete=models.CASCADE)
     text = models.CharField(max_length=255, verbose_name='質問文')
     question_type = models.CharField(max_length=2, choices=QUESTION_TYPE, default=TEXT)
     is_required = models.BooleanField(default=False, verbose_name="必須質問")
@@ -21,18 +31,21 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, related_name="choices", on_delete=models.CASCADE)
-    text = models.CharField(max_length=255)
+    text1 = models.CharField(max_length=255)    # 1つ目の選択肢
+    text2 = models.CharField(max_length=255)    # 2つ目の選択肢
+    text3 = models.CharField(max_length=255)    # 3つ目の選択肢
 
     def __str__(self):
-        return self.text
+        return f"{self.question} - {self.text1}, {self.text2}, {self.text3}"
 
-class Survey(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
-    questions = models.ManyToManyField(Question)
+class Rating(models.Model):
+    question = models.ForeignKey(Question, related_name="rating", on_delete=models.CASCADE)
+    text1 = models.CharField(max_length=255)    # 1つ目の評価軸
+    text2 = models.CharField(max_length=255)    # 2つ目の評価軸
+    text3 = models.CharField(max_length=255)    # 3つ目の評価軸
 
     def __str__(self):
-        return self.title
+        return f"{self.question} - {self.text1}, {self.text2}, {self.text3}"
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
