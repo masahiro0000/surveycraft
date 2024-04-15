f1dd9d7a94797a61bd2c68f22e9c7cb73f6fefd6
masahiro0000 <aya0724masa@gmail.com>
2024-04-15 11:54:36 +0900

    URLをsurveysからmy-surveyに変更

--- survey/urls.py
+++ survey/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('surveys/', views.surveys, name="surveys"),
     path('create/', views.create, name="create"),
     path('my-survey/', views.my_survey, name="my-survey"),
 ]
