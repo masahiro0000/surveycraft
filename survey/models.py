97a5aee647b9ad58306bbafae305cde10505a962
masahiro0000 <aya0724masa@gmail.com>
2024-04-17 10:03:23 +0900

    surveyモデル変更後、マイグレーション実施

--- survey/models.py
+++ survey/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from accounts.models import Profile
 
 class Survey(models.Model):
-    user = models.ForeignKey(Profile, related_name="surveys", on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, related_name="surveys", on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
 
